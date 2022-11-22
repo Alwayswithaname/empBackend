83105f3d91621c8ec74ae4e34b6b5aa7f7fcf685
whl <wplovitz@gmail.com>
2022-11-21 16:38:56 -0800

    added class files and filled out constructor labes

---
+++ classes/Role.js
@@ -0,0 +1,9 @@
+class Role {
+    constructor(tittle, salary, department_id) {
+        this.tittle = tittle;
+        this.salary = salary;
+        this.department_id = department_id;
+    }
+}
+
+module.exports = Role;
---
+++ index.js
@@ -0,0 +1,7 @@
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const commaNumber = require('comma-number');
+const Department = require(__dirname + '/')
+const Role = require(__dirname + '/')
+const Employee = require(__dirname + '/')
